0beae01f5b89a8704ba4cf59db8c4b4af2ad2576
zxr <514144669@qq.com>
2015-10-08 16:08:05 +0800

    Add Project

---
+++ TestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestDemo
+//
+//  Created by zxr on 15-10-8.
+//  Copyright (c) 2015年 com.iphoneblank.test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
